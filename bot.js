057fc52246c2920fff54b7f72aa071f0d5f5b972
Black Cat Neko <31685571+Blackwerecat@users.noreply.github.com>
2018-01-20 18:30:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@
 // @downloadUrl  https://raw.githubusercontent.com/Blackwerecat/Shard/master/bot.js
 // @description  Shard Bot
 // @author       You
-// @match        http://*/*
+// @match        https://hack.chat/*
 // @grant        none
 // ==/UserScript==
 
@@ -39,21 +39,27 @@
     }; //Event binder
 
     const Shard = { //Shard bot
-        init: () => {
+        init: function() {
             console.log("Setting up Shard...");
             console.log("Checking setup...");
-            let check_list = ['on-message','send-message'];
+            let check_list = ['send-message'];
             let flag = false;
             for (let check in check_list)
                 if (!find(check)) flag=true;
-
             if (flag)
                 console.log("Could not start Shard");
             else
                 console.log("Starting Shard...");
         },
-        find: (name) => {
-            if (this[name]) {console.log("Found "+name);return this[name];} else {console.log("Could not find "+name); return undefined;}
+        find: function(name) {
+            console.log(this);
+            if (this[name]) {
+                console.log("Found "+name);
+                return this[name];
+            }else {
+                console.log("Could not find "+name);
+                return undefined;
+            }
         }
     };
     window.EVENTS = EVENTS; //global
