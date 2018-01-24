5b232d5f529df732ab98038356ecb993a6ecf452
Black Cat Neko <31685571+Blackwerecat@users.noreply.github.com>
2018-01-23 16:21:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,100 +1,39 @@
 // ==UserScript==
-// @name         Shard
+// @name         Hack Chat Shard Interface
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @updateUrl    https://raw.githubusercontent.com/Blackwerecat/Shard/master/bot.js
-// @downloadUrl  https://raw.githubusercontent.com/Blackwerecat/Shard/master/bot.js
-// @description  Shard Bot
+// @description  try to take over the world!
 // @author       You
 // @match        https://hack.chat/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+setTimeout(function() {
     'use strict';
+    if (!window.opener) {
+        const shard_window = open(location.href);
+        alert('Shard is loading...');
+    }else {
+        shard.send_message = function(payload) {
+            send({cmd:'chat',text:payload.text});
+        };
+        shard.init();
 
-    const EVENTS = {
-        bindings:{},
-        bind:function(event,func){
-            if (this.bindings[event])
-                this.bindings[event].push(func);
-            else
-                this.bindings[event]=[func];
-            return this.bindings[event]?event+":"+this.bindings[event].length-1:null;
-        },
-        emit:function(event,payload){
-            if(this.bindings[event]) {
-                for (let i in this.bindings[event]) {
-                    let func = this.bindings[event][i];
-                    setTimeout(() => (func(payload)),0);
-                }
-            }
-        },
-        unbind:function(id){
-            var event = id.split(':')[0];
-            var index = id.split(':')[1];
-            if (event && index) {
-                this.bindings[event] = this.bindings[event].splice(index,index);
-            }
-        }
-    }; //Event binder
+        shard.setRank(opener.myNick,'admin'); //set my rank to admin
 
-    const Shard = { //Shard bot
-        init: function() {
-            console.log("Setting up Shard...");
-            console.log("Checking setup...");
-            let check_list = ['send_message'];
-            let flag = false;
-            for (let check in check_list) {
-                if (!this.find(check_list[check]))
-                    flag=true;
+        var _onmessage = ws.onmessage;
+        ws.onmessage = function(payload) {
+            let data = JSON.parse(payload.data);
+            console.log(data);
+            if (data.cmd=='chat' && data.nick != 'Shard') {
+                data.room = myChannel;
+                EVENTS.emit('on-message',data);
             }
-            if (flag)
-                console.log("Could not start Shard");
-            else {
-                console.log("Starting Shard...");
-                EVENTS.bind('on-message',this.parse.bind(this)); //Extra .bind to keep this bound to Shard
-            }
-        }, //Initialization and debugging
-        find: function(name) {
-            if (this[name]) {
-                console.log("Found "+name);
-                return this[name];
-            }else {
-                console.log("Could not find "+name);
-                return undefined;
-            }
-        },
 
-        commands: {
-            '-echo':{auth:'user',run:function(payload) {
-                this.send_message({text:payload.args.join(' '),room:payload.origin.room});
-            }},
-            '-setrank':{auth:'admin',run:function(payload) {
-                setRank(payload.args[0],payload.args[1]);
-            }}
-        },
+            _onmessage(payload);
+        };
 
-        ranks: {}, //Autherization
-        hierarchy: ['user','admin'],
-        setRank: function(name,rank) {if (this.hierarchy.contains(rank)) ranks[name]=rank;},
-        isAutherized: function(rank,auth) {
-            let rank_val = this.hierarchy.indexOf(rank);
-            let auth_val = this.hierarchy.indexOf(auth);
-            if (rank_val>-1 && auth_val>-1)
-                return rank_val>auth_val;
-            else
-                return undefined;
-        },
 
-        parse: function(payload) {
-            let text_array = payload.text.split(' ');
-            let cmd = text_array.shift();
-            let command = this.commands[cmd];
-            if (command && this.isAutherized(this.ranks[payload.nick],command.auth))
-                command.run.bind(this)({args:text_array,origin:payload});
-        }
-    };
-    window.EVENTS = EVENTS; //global
-    window.shard = Shard;
-})();
+        send({cmd:'join',channel:myChannel,nick:'Shard_Bot'});
+    }
+},2000);
